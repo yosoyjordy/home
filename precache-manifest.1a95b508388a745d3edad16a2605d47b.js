b802ffd44efeccb135854ffb0dc01cc9f5008473
yosoyjordy <yosoyjordy@hotmail.com>
2020-09-02 16:13:20 -0600

    Updates

---
+++ precache-manifest.1a95b508388a745d3edad16a2605d47b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b0e659d8526f161adb58a9ed3a0c041",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "7fb0df63d86a3d23f232",
+    "url": "/home/static/css/main.cd74b1f3.chunk.css"
+  },
+  {
+    "revision": "f37ea9a499db8716fc3b",
+    "url": "/home/static/js/2.2db01562.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/home/static/js/2.2db01562.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fb0df63d86a3d23f232",
+    "url": "/home/static/js/main.93b8886c.chunk.js"
+  },
+  {
+    "revision": "6a22e175bf871529e9e6",
+    "url": "/home/static/js/runtime-main.25ac49be.js"
+  },
+  {
+    "revision": "bc889db751fdf75774f1be610a501c1d",
+    "url": "/home/static/media/ajax.bc889db7.png"
+  },
+  {
+    "revision": "2ce974a89566f44d22732b67e0310e6e",
+    "url": "/home/static/media/coding.2ce974a8.png"
+  },
+  {
+    "revision": "1ac1c7612f52f49bad632e73c4f8a604",
+    "url": "/home/static/media/css.1ac1c761.png"
+  },
+  {
+    "revision": "6af009634dc82ff6f98ee2d029d81d6b",
+    "url": "/home/static/media/cv.6af00963.pdf"
+  },
+  {
+    "revision": "c00a202d4ea741c7486369a91fad1c28",
+    "url": "/home/static/media/html.c00a202d.png"
+  },
+  {
+    "revision": "e57b2996aa77463a4feac5ab0a04401d",
+    "url": "/home/static/media/jquery.e57b2996.png"
+  },
+  {
+    "revision": "babb7fa37b7e1acd172a5e8c42a4c476",
+    "url": "/home/static/media/js.babb7fa3.png"
+  },
+  {
+    "revision": "34d32b20341f07fb6039e3a2dbed3460",
+    "url": "/home/static/media/mongodb.34d32b20.png"
+  },
+  {
+    "revision": "7171937ca1f8565112f26c7d32dbc870",
+    "url": "/home/static/media/mysql.7171937c.png"
+  },
+  {
+    "revision": "b68979e9bd12108a01a5e026440de5d7",
+    "url": "/home/static/media/nodejs.b68979e9.png"
+  },
+  {
+    "revision": "3c6b67880fdb1b164042a21b6f19e98f",
+    "url": "/home/static/media/php.3c6b6788.png"
+  },
+  {
+    "revision": "9ad39b47f8d7d44bac0b474a6105a08f",
+    "url": "/home/static/media/profile.9ad39b47.jpg"
+  },
+  {
+    "revision": "43def38f0effef61b2d7012298b383a2",
+    "url": "/home/static/media/wpress.43def38f.png"
+  }
+]);
